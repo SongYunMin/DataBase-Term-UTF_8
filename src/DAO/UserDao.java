88504c0f51baa48f40097d198967460b81f46ad6
YunMin-Song <thd930308@naver.com>
2020-11-21 13:40:24 +0900

    DAO/DTO Sql Update

--- src/DAO/UserDao.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-public class UserDao {
-}
---
+++ src/DAO/UserInfoDao.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import DTO.UserInfoDto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserInfoDao {
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+
+    // DataBase Access
+    public UserInfoDao() {
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/BBS?serverTimezone=UTC";
+            String dbID = "root";
+            String dbPW = "";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int register(UserInfoDto dto){
+        String SQL = "INSERT INTO userinfo(UserName, UserID, UserPW) VALUES(?,?,?)";
+        try{
+            pstmt=conn.prepareStatement(SQL);
+            pstmt.setString(1,dto.getUserName());
+            pstmt.setString(2,dto.getUserID());
+            pstmt.setString(3,dto.getUserPW());
+            return pstmt.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return -1;      // DataBase Error
+    }
+
+}
